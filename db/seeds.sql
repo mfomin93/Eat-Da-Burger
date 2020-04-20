832fd3ddf850f2c6f32a3e5c8a0a104915d5074f
mfomin93 <mfomin93@gmail.com>
2020-04-20 14:42:38 -0400

    import from VS Code into Github

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES('All-American');
+
+INSERT INTO burgers(burger_name)
+VALUES('Bacon Cheeseburger');
+
+INSERT INTO burgers(burger_name)
+VALUES('New Burger Who Dis');
+
+INSERT INTO burgers(burger_name)
+VALUES('Southern Burger');
+
+INSERT INTO burgers(burger_name)
+VALUES('Veggie Burger');
