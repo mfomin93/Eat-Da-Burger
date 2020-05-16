0167f71840c0f04f0a4b6a58fa484ea993f3ff74
mfomin93 <mfomin93@gmail.com>
2020-05-15 23:03:01 -0400

    JawsDB connection fix

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "abcd1234",
-    database: "burgers_db"
+    user: "ximi1ukwgoki0h8g",
+    password: "ne8uvanpoyrrd55i",
+    database: "uatwmtlunzsuwt4o"
   });
 };
 
